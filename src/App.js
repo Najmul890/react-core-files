cf686a5f05cac9fad199a02bc535dbbf8fa2d4d1
Najmul890 <najmul890890@gmail.com>
2021-04-09 14:05:34 +0600

    adding files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <h2>This is React App</h2>
+       <p>my react</p>
+       
+       
+    </div>
+  );
+}
+
+export default App;
